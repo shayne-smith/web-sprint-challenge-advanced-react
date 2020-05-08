c4ff2bcd7b673221f3788c7ab8a41e580067133c
seanmx96 <seanmx96@users.noreply.github.com>
2020-05-08 11:15:52 -0500

    Setup 2 tests for header and success message rendering.

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import { useForm } from '../hooks/useForm';
+
 const initialValue = {
   firstName: "",
   lastName: "",
@@ -15,11 +17,11 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [values, handleChanges, clearForm] = useForm('checkoutform', initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  // const handleChanges = (e) => {
+  //   setValues({ ...values, [e.target.name]: e.target.value });
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -66,7 +68,7 @@ const CheckoutForm = (props) => {
           Zip:
           <input name="zip" value={values.zip} onChange={handleChanges} />
         </label>
-        <button>Checkout</button>
+        <button data-testid="checkout">Checkout</button>
       </form>
 
       {showSuccessMessage && (
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,38 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, getByLabelText, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const { getByText } = render(<CheckoutForm />);
 
-test("form shows success message on submit with form details", () => {});
+    const header = getByText(/Checkout Form/i);
+
+    expect(header).toBeInTheDocument();
+});
+
+test("form shows success message on submit with form details", async () => {
+    const { getByText, getByLabelText, getByTestId } = render(<CheckoutForm />);
+
+    const firstNameInput = getByLabelText(/first name/i);
+    const lastNameInput = getByLabelText(/last name/i);
+    const addressInput = getByLabelText(/address/i);
+    const cityInput = getByLabelText(/city/i);
+    const stateInput = getByLabelText(/state/i);
+    const zipInput = getByLabelText(/zip/i);
+    const checkoutButton = getByTestId(/checkout/i);
+
+    fireEvent.change(firstNameInput, { target: { value: 'Shayne' } });
+    fireEvent.change(lastNameInput, { target: { value: 'Smith' } });
+    fireEvent.change(addressInput, { target: { value: '9004 Waltlee Road' } });
+    fireEvent.change(cityInput, { target: { value: 'Louisville' } });
+    fireEvent.change(stateInput, { target: { value: 'KY' } });
+    fireEvent.change(zipInput, { target: { value: '40291' } });
+    fireEvent.click(checkoutButton);
+
+    await waitFor(() => {
+        const successMessage = getByText(/You have ordered some plants!/i);
+        expect(successMessage).toBeInTheDocument();
+    });
+});
